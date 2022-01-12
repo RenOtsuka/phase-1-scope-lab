efaf3c668f7c337ca097b64195af39d773edd7bc
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-01-12 11:41:04 -0600

    commit tree 7cba2c2e72057f9089a1b05f416e0bc49d9d3a5f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    console.log(customerName.toUpperCase());
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(name){
+    bestCustomer = name;
+}
+
+const leastFavoriteCustomer = 'rob';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bill';
+}
